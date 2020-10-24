246b5c8bd5d2eb0cd64b35330ea2fd10f7175049
GianShihab <gianshihabkhan12@gmail.com>
2020-10-24 02:39:12 -0700

    Task Praktikum

---
+++ Praktikum/Praktikum 1/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class Perkenalan extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan<h1>";
+		echo "<h3>Nama saya Amaliyah Gian Fadilah<br>
+			Saya tinggal di Tegal<br>
+			Saya suka membaca novel<h3>";
+	}
+}
